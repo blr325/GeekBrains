d70919ad4b0c23eec4ddba8abb1109278592cb93
Andrei <surginevich1998@gmail.com>
2022-02-06 23:04:20 +0300

    completed lesson 2

---
+++ src/main/java/java1/lesson2/lesson2.java
@@ -0,0 +1,36 @@
+package java1.lesson2;
+
+public class lesson2 {
+    public static void main(String[] args) {
+        System.out.println(checkSum(6, 5));
+        checkInt(-1);
+        System.out.println(isCheckInt(-1));
+        printLine("Строка ", 5);
+        System.out.println(isCheckLeapYear(2000));
+    }
+
+    public static boolean isCheckLeapYear(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
+    public static boolean checkSum(int a, int b) {
+        return a + b >= 10 && a + b <= 20;
+    }
+
+    public static void checkInt(int a) {
+        if (a >= 0) {
+            System.out.println("Число положительное");
+        } else System.out.println("Число отрицательное");
+    }
+
+    public static boolean isCheckInt(int a) {
+        return a < 0;
+    }
+
+    public static void printLine(String line, int num) {
+        for (int i = 1; i <= num; ++i) {
+            System.out.println(line + i);
+        }
+    }
+}
+
