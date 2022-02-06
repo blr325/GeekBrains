f1c80ecc44bfb45bc2b67097559fc114725e08d0
Andrei <surginevich1998@gmail.com>
2022-02-06 21:55:39 +0300

    completed the lesson 1

---
+++ src/main/java/java1/lesson1/HomeWorkApp.java
@@ -0,0 +1,42 @@
+package java1.lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    private static void compareNumbers() {
+        int a = 10, b = 10;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else System.out.println("a < b");
+    }
+
+    private static void printColor() {
+        int value = 10;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else System.out.println("Зеленый");
+    }
+
+    private static void checkSumSign() {
+        int a = 4, b = 5;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else
+            System.out.println("Сумма отрицательная");
+    }
+
+
+    private static void printThreeWords() {
+        System.out.print("Orange\nBanana\nApple\n");
+    }
+}
+
+
+
